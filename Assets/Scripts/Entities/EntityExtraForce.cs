3020137d0cc9021a85b6200df8a01cc827607f29
Niels <niels.poelder@gmail.com>
2019-09-12 17:13:06 +0200

    Bugfixes and Optimizations

--- Assets/Scripts/Entities/EntityExtraForce.cs
+++ Assets/Scripts/Entities/EntityExtraForce.cs
@@ -24,7 +24,7 @@ public class EntityExtraForce : Entity
                 rb.AddRelativeForce(_Vector);
             }
         }
-        Destroy(this.gameObject);
+        gameObject.SetActive(false);
         
     }
 }
